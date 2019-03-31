ebf8b496883464abf416be60287a6deb84185c06
Jean Tampon <jean.tampon@gmail.com>
2019-03-31 11:54:55 +0200

    Start implementation

--- jsonparser.hpp
+++ jsonparser.hpp
@@ -8,9 +8,10 @@ namespace jsp
 	/*
 		Base class for all kind of JSON objects
 	*/
+
 	struct JsonBase
 	{
-		JsonBase();
+		JsonBase() = default;
 	};
 
 	struct JsonData : public JsonBase
@@ -57,14 +58,24 @@ namespace jsp
 			return r;
 		}
 
+		const std::string& asString() const
+		{
+			return m_data;
+		}
+
 		std::string m_data;
 	};
 
 	struct JsonObject : public JsonBase
 	{
-		JsonObject();
+		JsonObject() :
+			JsonBase()
+		{}
 
-		void add(const std::string& key, const JsonObject& child);
+		void add(const std::string& key, const JsonObject& child)
+		{
+			m_childs[key] = child;
+		}
 
 		std::string m_value;
 		std::map<std::string, JsonObject> m_childs;
@@ -78,4 +89,65 @@ namespace jsp
 
 		std::vector<JsonObject> m_items;
 	};
+
+	bool getIdentifier(const std::string& raw_data, uint32_t start, uint32_t& id_start, uint32_t& id_length)
+	{
+		bool found(false);
+		uint32_t size(raw_data.size());
+		for (int i(start); i < size; ++i)
+		{
+			const char c(raw_data[i]);
+			if (!found)
+			{
+				if (c == '}')
+				{
+					return false;
+				}
+				else if (c != ':' && c != '\n')
+				{
+					found = true;
+					id_start = i;
+				}
+				else if(!found && c == ':')
+				{
+					return false;
+				}
+			}
+			else if (found && c == ':')
+			{
+				id_length = i - id_start;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	JsonObject parse(const std::string& raw_data, uint32_t start, uint32_t& end)
+	{
+		JsonObject root;
+		uint32_t size(raw_data.size());
+		if (!size)
+			return root;
+		
+		for (uint32_t i(start); i < size; ++i)
+		{
+			const char c(raw_data[i]);
+			if (c == '{')
+			{
+				uint32_t id_start, id_length;
+				if (getIdentifier(raw_data, i + 1, id_start, id_length))
+				{
+					root.add(raw_data.substr(id_start, id_length), parse(raw_data, id_start + id_length, i));
+				}
+			}
+			else if (c == '}')
+			{
+				end = i + 1;
+				break;
+			}
+		}
+
+		return root;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "jsonparser.hpp"
 
 int main()
 {
+	uint32_t end;
+	jsp::JsonObject root = jsp::parse("{}", 0, end);
 	return 0;
 }
